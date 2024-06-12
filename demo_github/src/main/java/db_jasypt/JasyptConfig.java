517cbb0fd33594909cb329be51f407aa8e15ad1d
Notebook9 <bluejeansh@hanmail.net>
2024-06-12 17:28:37 +0900

    db정보 암호화2

--- demo_github/src/main/java/db_jasypt/JasyptConfig.java
+++ demo_github/src/main/java/db_jasypt/JasyptConfig.java
@@ -21,22 +21,12 @@ public class JasyptConfig {
 	public StringEncryptor stringEncryptor(){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        //config.setPassword("1234");
         config.setPassword(environment.getProperty("jasypt.encryptor.password"));
-        //application.properties + JasyptConfig.java --> github push
-        //암호화비번-직접 소스상 기록->외부환경설정변수값 읽어오자
-        //자바클래스 실행-환경변수 읽어서 사용
-        //-Djasypt.encryptor.password=1234
+
 
         config.setPoolSize("1");
         encryptor.setConfig(config);
-        System.out.println("=== JasyptConfig 실행결과 ===");
-        System.out.println(encryptor.decrypt("FqmbbYNR1aomoaWZffcpEAQrUGBgLKDe92S9+ol0Nkj+UNaysDnF9g=="));
-        System.out.println(encryptor.decrypt("2oLm+I2F0Ln07Phtxgs43xBVjgK1yWbC/lLdjsBhbA+s9a8ndzjJQ0v4EyknOgM6"));
-        System.out.println(encryptor.decrypt("nZ9WjYcw76FaW6KeCA4GuQG6p+Wblz4G"));
-        System.out.println(encryptor.decrypt("6HpuqwUU1WTtfa/YdbWccM6seGPrIlhK"));
-        System.out.println("=========================");
-        System.out.println(encryptor);
+ 
         return encryptor;
 	}
 	
